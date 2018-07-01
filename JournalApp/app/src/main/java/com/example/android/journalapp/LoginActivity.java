45695b6c4fc75bc1e4876b8ac6845d6b05db280d
pamsy <pamelaorline@gmail.com>
2018-07-01 23:20:41 +0100

    i have to submit but design is not fun

--- JournalApp/app/src/main/java/com/example/android/journalapp/LoginActivity.java
+++ JournalApp/app/src/main/java/com/example/android/journalapp/LoginActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.text.BreakIterator;
 import java.util.Arrays;
 import java.util.List;
 
@@ -48,8 +49,34 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
+//    @Override
+//    public void onStart(){
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+//    }
+
+//    private void updateUI(FirebaseUser user) {
+//        hideProgressDialog();
+//
+//        if (user != null) {
+//            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+//            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+//
+//            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+//        } else {
+//            mStatusTextView.setText(R.string.signed_out);
+//            mDetailTextView.setText(null);
+//
+//            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+//        }
+//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
